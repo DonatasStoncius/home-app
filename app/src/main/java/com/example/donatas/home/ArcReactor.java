dcade28940d15aa8c998314caf4a48adf8272f14
Donatas Stončius <donatas93@gmail.com>
2017-08-16 22:20:12 +0300

    First commit of the app

---
+++ app/src/main/java/com/example/donatas/home/ArcReactor.java
@@ -0,0 +1,22 @@
+package com.example.donatas.home;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Donatas- on 2017-07-25.
+ */
+
+public class ArcReactor extends AppCompatActivity {
+    public static final int REQUEST_ENABLE_BT = 42;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+}
